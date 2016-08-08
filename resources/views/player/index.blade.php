ef597f492aaf5ec2dde2884ca441e71bfc605252
Jake Davies <jdavies.thfc@gmail.com>
2016-08-08 18:25:20 +0100

    bower, and star ratings

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -1,1 +1,23 @@
-Hi!
+@extends('layouts.master')
+
+@section('title', 'All Players')
+
+@section('content')
+    <div class="row">
+        @foreach ($players as $player)
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <h3>{{ $player->name }}</h3>
+                    <img src="..." alt="...">
+                    <div class="caption">
+                        <script>
+                            $("#input-id").rating();
+                        </script>
+                        <input id="input-id" name="input-name" type="number" class="rating" min=0 max=5 step=1 data-size="lg" data-rtl="false">
+                        <p><a href="#" class="btn btn-primary" role="button">Rate</a></p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
